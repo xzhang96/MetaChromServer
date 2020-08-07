042ecd1bbdb952637b39d9e58c72d56486652535
Cindy Zhang <xz2582@columbia.edu>
2020-08-06 22:34:52 -0500

    added submit validation

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import time
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -8,6 +9,12 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 def index():
     return render_template('homepage.html')
 
+
+@app.route('/results', methods=['GET', 'POST'])
+def get_info():
+    if request.method == 'POST':
+        fileType = request.form['fileType']
+    return fileType
 
 if __name__ == '__main__':
     app.run()
---
+++ static/script.js
@@ -0,0 +1,39 @@
+function ShowVarSelection() {
+    var variant = document.getElementById("var");
+    var selectVar = document.getElementById("selectVar");
+    selectVar.style.display = variant.checked ? "block" : "none";
+}
+function ShowUploadMethod() {
+    var method1 = document.getElementById("paste");
+    var displayMethod1 = document.getElementById("textInputFile");
+    displayMethod1.style.display = method1.checked ? "block" : "none";
+    var method2 = document.getElementById("file");
+    var displayMethod2 = document.getElementById("uploadInputFile");
+    displayMethod2.style.display = method2.checked ? "block" : "none";
+}
+function validate() {
+    if (document.getElementById("seq").checked || (document.getElementById("var").checked && document.getElementById("selectVar").value != '')) {
+        if (document.getElementById("paste").checked) {
+            if (document.getElementById("textInputFile").value != '') {
+                return true;
+            } else {
+                alert("input valid text")
+                return false;
+            }
+            
+        } else if (document.getElementById("file").checked) {
+            if (document.getElementById("uploadInputFile").files.length == 0) {
+                alert("no file selected");
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            alert("input file");
+            return false;
+        }
+    } else {
+        alert("file type")
+        return false;
+    }
+}
