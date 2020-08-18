0b9bfe8ed54604982469a50cea4bb782aaca76c0
Cindy Zhang <xz2582@columbia.edu>
2020-08-18 11:46:34 -0500

    edited

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import time
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'InputData/')
 
 @app.route('/')
 def index():
@@ -14,7 +16,16 @@ def index():
 def get_info():
     if request.method == 'POST':
         fileType = request.form['fileType']
-    return fileType
+        uploadMethod = request.form['inputFile']
+        if uploadMethod == 'file':
+            uploadFile = request.files['inputUploadFile']
+            # uploadFile.save(secure_filename(uploadFile.filename))
+            t = time.time()
+            fileName = str(t) + '_' + fileType + '_' + uploadFile.filename
+            uploadFile.save(os.path.join(UPLOAD_FOLDER, fileName))
+
+
+    return "file uploaded successfully"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
